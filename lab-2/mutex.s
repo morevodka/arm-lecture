317c3e127d0c529e40fe309485b0b8c7f94187d4
morevodka <khs920205@gmail.com>
2014-09-28 02:38:19 +0800

    modified:   example1/example1.c 	modified:   lab-1/fib.s 	modified:   lab-2/mutex.s

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,7 +9,13 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
-
+        ldr r1,=locked @lock data
+    .L1:
+        ldrex r2,[r0] @load r0 exslusive
+        cmp r2,#0    @if(r2 load form [r0])
+           strexeq r2,r1,[r0]  @store [r0] to r1
+           cmpeq r2,#0
+           bne .L1   @not successful
         @ END CODE INSERT
 	bx lr
 
@@ -19,7 +25,8 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
-        
+        ldr r1,=unlocked @unlock data
+        str r1,[r0]     @store [r0] to r1
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
